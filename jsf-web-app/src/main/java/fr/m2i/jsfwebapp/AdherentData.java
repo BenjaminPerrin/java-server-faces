2aa73514f84d265840ea12082f4cf6523043685e
benjamin perrin <perrin.benjamin77@gmail.com>
2022-08-16 14:49:01 +0200

    correction

---
+++ jsf-web-app/src/main/java/fr/m2i/jsfwebapp/AdherentData.java
@@ -0,0 +1,45 @@
+package fr.m2i.jsfwebapp;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@SessionScoped
+@ManagedBean(name = "adherentData", eager = true)
+public class AdherentData {
+
+    private List<Adherent> adherents;
+    private Adherent adherent;
+    
+    public AdherentData() {
+        this.adherents = new ArrayList();
+        this.adherent = new Adherent();
+    }
+
+    public String goToValidate() {
+        return "validate?faces-redirect=true";
+    }
+
+    public String onSave() {
+        adherents.add(adherent);
+        adherent = new Adherent();
+        return "listAdherent";
+    }
+
+    public List<Adherent> getAdherents() {
+        return adherents;
+    }
+
+    public void setAdherents(List<Adherent> adherents) {
+        this.adherents = adherents;
+    }
+
+    public Adherent getAdherent() {
+        return adherent;
+    }
+
+    public void setAdherent(Adherent adherent) {
+        this.adherent = adherent;
+    }
+}
